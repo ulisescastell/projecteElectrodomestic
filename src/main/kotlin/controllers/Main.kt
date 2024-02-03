2822b4b79295e6db696299b563cf9bdeacfb39f8
ulisescastell <ulises.castell.7e7@itb.cat>
2024-02-03 14:32:22 +0100

    add new subclasses Rentadora, Televisio

--- src/main/kotlin/controllers/Main.kt
+++ src/main/kotlin/controllers/Main.kt
@@ -2,6 +2,23 @@ package controllers
 import models.*
 import utilities.*
 
-fun main(args: Array<String>) {
-
+fun main() {
+    val llistaElectrodomestics = arrayOf(
+        Electrodomestics(100.0,"Blanc", 'A', 6.0),
+        Electrodomestics(110.2,"Platejat", 'B', 12.0),
+        Electrodomestics(120.0,"Rosa", 'C', 24.0),
+        Electrodomestics(134.4,"Lila", 'D', 50.0),
+        Electrodomestics(200.0,"Blau", 'E', 190.0),
+        Electrodomestics(300.0,"Blau cel", 'F', 62.0),
+        Electrodomestics(140.5,"Vermell", 'A', 65.0),
+        Electrodomestics(130.0,"Blanc", 'C', 97.0),
+        Rentadora(106.2,"Negre", 'F', 34.0, 6),
+        Electrodomestics(500.7,"Blanc", 'A', 23.4)
+    )
+    for (i in 0..llistaElectrodomestics.size-1) {
+        println("\n---------------")
+        print("\nElectrodomèstic ${i+1}:")
+        llistaElectrodomestics[i].preuFinal()
+        println(llistaElectrodomestics[i])
+    }
 }
--- src/main/kotlin/models/Electrodomestics.kt
+++ src/main/kotlin/models/Electrodomestics.kt
@@ -3,34 +3,53 @@ import controllers.*
 import utilities.*
 
 open class Electrodomestics {
-    private var preuBase: Double = 0.0
-    private var color: Array<String> = arrayOf("")
-    private var consum: Char = ' '
-    private var pes: Double = 0.0
+    protected var preuBase: Double = 0.0
+    protected var color: String = ""
+    protected var consum: Char = ' '
+    protected var pes: Double = 0.0
+    protected var preuFinal: Double = 0.0
 
-    constructor(color: Array<String> , consum: Char, pes: Double) {
+    constructor(preuBase: Double, color: String , consum: Char, pes: Double) {
+        this.preuBase = preuBase
         this.color = color
         this.consum = consum
         this.pes = pes
     }
 
     fun preuFinal () {
-        when (this.consum) {
-            'A' -> this.preuBase+35.0
-            'B' -> this.preuBase+30.0
-            'C' -> this.preuBase+25.0
-            'D' -> this.preuBase+20.0
-            'E' -> this.preuBase+15.0
-            'F' -> this.preuBase+10.0
-            'G' -> this.preuBase+0.0
+        val valorPerConsum = when (this.consum) {
+            'A' -> 35.0
+            'B' -> 30.0
+            'C' -> 25.0
+            'D' -> 20.0
+            'E' -> 15.0
+            'F' -> 10.0
+            'G' -> 0.0
+            else -> {
+                println("Eficiencia energètica no disponible")
+                0.0
+            }
         }
+        val valorPerPes = when (this.pes) {
+            in 6.0..20.0 -> 20.0
+            in 20.1..50.0 -> 50.0
+            in 50.1..80.0 -> 80.0
+            in 80.1..200.0 -> 100.0
+            else -> {
+                println("Peso incorrecto")
+                0.0
+            }
+        }
+
+        this.preuFinal = preuBase+valorPerPes+valorPerConsum
     }
+
 
     fun getPreuBase (): Double {
         return this.preuBase
     }
 
-    fun getColor(): Array<String> {
+    fun getColor(): String {
         return this.color
     }
 
@@ -46,7 +65,7 @@ open class Electrodomestics {
         this.preuBase = preuBase
     }
 
-    fun setColor(color: Array<String>) {
+    fun setColor(color: String) {
         this.color = color
     }
 
@@ -56,5 +75,44 @@ open class Electrodomestics {
 
     fun setPes(pes: Double) {
         this.pes = pes
+    }
+
+    override fun toString(): String {
+        return "\nPreu base: ${this.preuBase}\nColor: ${this.color}\nConsum: ${this.consum}\nPes: ${this.pes}\nPreu final: ${this.preuFinal}"
+    }
+}
+
+class Rentadora : Electrodomestics {
+    private var carrega:Int = 0
+    constructor(preuBase: Double, color: String, consum: Char, pes: Double, carrega: Int): super(preuBase, color, consum, pes) {
+        this.carrega = carrega
+    }
+
+    fun getCarrega(): Int {
+        return this.carrega
+    }
+    fun setCarrega(carrega: Int) {
+        this.carrega = carrega
+    }
+    override fun toString(): String {
+        return "\nPreu base: ${this.preuBase}\nColor: ${this.color}\nConsum: ${this.consum}\nPes: ${this.pes}\nPreu final: ${this.preuFinal}\nCàrrega: ${this.carrega}"
+    }
+    }
+
+
+class Televisio : Electrodomestics {
+    private var mida:Int = 0
+    constructor(preuBase: Double, color: String, consum: Char, pes: Double, mida: Int): super(preuBase, color, consum, pes) {
+        this.mida = mida
+    }
+
+    fun getMida(): Int {
+        return this.mida
+    }
+    fun setMida(mida: Int) {
+        this.mida = mida
+    }
+    override fun toString(): String {
+        return "\nPreu base: ${this.preuBase}\nColor: ${this.color}\nConsum: ${this.consum}\nPes: ${this.pes}\nPreu final: ${this.preuFinal}\nMida: ${this.mida}"
     }
 }
